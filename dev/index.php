e3f1d9604f89addcd0ddf33889bbf303a1d63794
BrenDt <brent@stitcher.io>
2017-02-06 12:05:51 +0100

    Add Github ribbon

--- dev/index.php
+++ dev/index.php
@@ -2,7 +2,7 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-use brendt\stitcher\controller\DevController;
+use Brendt\Stitcher\Controller\DevController;
 
 // This controller will render HTML pages on the fly.
 // See config.dev.yml for more information.
