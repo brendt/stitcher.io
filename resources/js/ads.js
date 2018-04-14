3555adc108d3aaadf3ede5bc36ff47c83606016f
Brent Roose <brent@spatie.be>
2018-04-14 15:39:46 +0200

    update

--- resources/js/ads.js
+++ resources/js/ads.js
@@ -2,4 +2,5 @@ window.adsEnabled = true;
 
 document.addEventListener('DOMContentLoaded', function () {
     document.querySelector('.sneaky-placeholder').classList.remove('shown');
+    console.log('hi');
 });
