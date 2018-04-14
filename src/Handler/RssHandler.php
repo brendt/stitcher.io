f2b3e06a4c9ea8b112e040928f14f595928c7941
Brent Roose <brent@spatie.be>
2018-04-14 11:48:40 +0200

    Add rss feed

---
+++ src/Handler/RssHandler.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Brendt\Stitcher\Handler;
+
+use GuzzleHttp\Psr7\Response;
+use Pageon\Http\Header;
+use Stitcher\App;
+use Stitcher\File;
+use Stitcher\Renderer\RendererFactory;
+use Stitcher\Variable\VariableParser;
+use Stitcher\Variable\YamlVariable;
+use Symfony\Component\Yaml\Yaml;
+
+class RssHandler
+{
+    /** @var \Stitcher\Renderer\Renderer */
+    private $renderer;
+
+    public function __construct()
+    {
+        /** @var RendererFactory $rendererFactory */
+        $rendererFactory = App::get(RendererFactory::class);
+
+        $this->renderer = $rendererFactory->create();
+    }
+
+    public function handle(): Response
+    {
+        $posts = YamlVariable::make(
+            File::path('src/content/blog.yaml'),
+            App::get(Yaml::class),
+            App::get(VariableParser::class)
+        )->getParsed();
+
+        $rss = $this->renderer->renderTemplate(
+            'rss.twig',
+            ['posts' => $posts]
+        );
+
+        return new Response(200, [Header::make('Content-Type', 'application/xml;charset=UTF-8')], $rss);
+    }
+}
--- src/routes.php
+++ src/routes.php
@@ -1,5 +1,7 @@
 <?php
 
+use Brendt\Stitcher\Handler\RssHandler;
+
 $redirects = [
     '/blog' => '/blog/page-1',
     '/guide' => '/guide/setting-up',
@@ -11,3 +13,5 @@ $router = \Stitcher\App::router();
 foreach ($redirects as $url => $targetUrl) {
     $router->redirect($url, $targetUrl);
 }
+
+$router->get('/rss', RssHandler::class);
