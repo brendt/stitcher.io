d54f8eb35d9cefea23731aa878a2072a8d8fab14
Brent Roose <brent@spatie.be>
2018-04-15 14:18:54 +0200

    Add redirects

--- src/routes.php
+++ src/routes.php
@@ -6,6 +6,8 @@ $redirects = [
     '/blog' => '/blog/page-1',
     '/guide' => '/guide/setting-up',
     '/cognitive-load' => '/blog/a-programmers-cognitive-load',
+    '/key-binding' => '/blog/mastering-key-bindings',
+    '/curly' => '/blog/where-a-curly-bracket-belongs',
 ];
 
 $router = \Stitcher\App::router();
