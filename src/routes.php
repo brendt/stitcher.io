3eae27f4a041fe23b7b4f63afcfb1b6d9a7c0008
Brent Roose <brent@spatie.be>
2018-04-17 21:45:12 +0200

    Update styling

--- src/routes.php
+++ src/routes.php
@@ -3,6 +3,7 @@
 use Brendt\Stitcher\Handler\RssHandler;
 
 $redirects = [
+    '/feed' => '/rss',
     '/blog' => '/blog/page-1',
     '/guide' => '/guide/setting-up',
     '/cognitive-load' => '/blog/a-programmers-cognitive-load',
