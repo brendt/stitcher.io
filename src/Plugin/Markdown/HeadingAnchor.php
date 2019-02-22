1198200023a103444d1adbecad2216dbea51d66d
Brent Roose <brent@spatie.be>
2019-02-22 15:32:56 +0100

    Add heading anchors

--- src/Plugin/Markdown/HeadingAnchor.php
+++ src/Plugin/Markdown/HeadingAnchor.php
@@ -5,7 +5,6 @@ namespace Brendt\Stitcher\Plugin\Markdown;
 use League\CommonMark\Block\Element\AbstractBlock;
 use League\CommonMark\Block\Renderer\HeadingRenderer;
 use League\CommonMark\ElementRendererInterface;
-use League\CommonMark\HtmlElement;
 
 class HeadingAnchor extends HeadingRenderer
 {
@@ -16,6 +15,12 @@ class HeadingAnchor extends HeadingRenderer
         $slug = $this->slug($block->getContents());
 
         $block->setAttribute('id', $slug);
+
+        $contents = $block->getContents();
+
+        $contents = "<a href=\"#{$slug}\" class=\"heading-anchor\">#</a> {$contents}";
+
+        $block->setContents($contents);
 
         return $block;
     }
