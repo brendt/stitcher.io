ff57b8e082bc6005eebe208cda07ce762f7c5693
Brent Roose <brent.roose@gmail.com>
2023-07-04 10:51:12 +0200

    wip

--- src/Plugin/Twig/ImageExtension.php
+++ src/Plugin/Twig/ImageExtension.php
@@ -2,17 +2,29 @@
 
 namespace Brendt\Stitcher\Plugin\Twig;
 
+use Pageon\Html\Image\ImageFactory;
 use Stitcher\Renderer\Extension;
 
 class ImageExtension implements Extension
 {
+    private ImageFactory $imageFactory;
+
+    public function __construct(ImageFactory $imageFactory)
+    {
+        $this->imageFactory = $imageFactory;
+    }
+
     public function name(): string
     {
         return 'image';
     }
 
-    public function path(string $path): string
+    public function render(string $path): string
     {
-        return 'HELLO';
+        $responsiveImage = $this->imageFactory->create($path);
+
+        return <<<HTML
+        <img src="{$responsiveImage->src()}" srcset="{$responsiveImage->srcset()}" sizes="{$responsiveImage->sizes()}" alt="" />
+        HTML;
     }
 }
