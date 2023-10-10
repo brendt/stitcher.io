e696a97238ec60152c63732e0672324c711b9fa8
Brent Roose <brent.roose@gmail.com>
2023-10-10 11:29:15 +0200

    wip

--- src/config.php
+++ src/config.php
@@ -23,6 +23,7 @@ return [
     'staticFiles' => [
         'resources/img/static/',
         'resources/img/static/php-in-7-minutes-thumb.jpg',
+        'resources/img/static/83-thumb.png',
         'resources/img/static/generics-thumb-1.png',
         'resources/img/static/generics-thumb-2.png',
         'resources/img/static/generics-thumb-3.png',
