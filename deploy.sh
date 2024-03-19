b99c2ee5d48200d553bce00f4a15b8491952c0a9
brendt <brent.roose@gmail.com>
2024-03-19 05:13:06 +0100

    WIP

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
-ssh forge@stitcher.io '\
-  cd stitcher.io \
-  git pull \
-  php8.3 /usr/local/bin/composer install \
-  php8.3 stitcher.php \
+ssh forge@stitcher.io '
+  cd "stitcher.io"; \
+  git pull; \
+  php8.3 /usr/local/bin/composer install; \
+  php8.3 stitcher.php; \
+  echo "done"; \
 '
