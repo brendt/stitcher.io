0c6f1962e20c9bd33c6a88fba68821e3cefabdba
brendt <brent.roose@gmail.com>
2025-10-21 14:50:20 +0200

    wip

--- app/ViewComponents/x-meta-image.view.php
+++ app/ViewComponents/x-meta-image.view.php
@@ -9,7 +9,7 @@
     </style>
 </head>
 <body class="bg-[#333] flex justify-center items-center">
-<div class="w-[1200px] h-[628px] flex justify-center items-center bg-primary relative text-white p-1">
+<div class="w-full h-full flex justify-center items-center bg-primary relative text-white p-1">
     <div class="w-full h-full bg-white rounded-xs text-primary flex justify-around items-center relative shadow-lg px-8">
         <x-slot/>
 
