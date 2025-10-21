5eb83ac3b213b23f93502c4d152721cca005594e
brendt <brent.roose@gmail.com>
2025-10-21 13:50:31 +0200

    wip

--- app/ViewComponents/x-footer.view.php
+++ app/ViewComponents/x-footer.view.php
@@ -1,6 +1,8 @@
 <x-card class="
         grid gap-4
         p-4 sm:py-4 sm:px-0
+        sm:bg-transparent
+        sm:shadow-none
     ">
     <div class="flex flex-wrap flex-col-reverse sm:flex-row justify-center gap-2 items-center sm:flex sm:justify-between">
         <x-slot/>
