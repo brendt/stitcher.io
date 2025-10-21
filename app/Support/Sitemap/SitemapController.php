8a3101a2d8e6a429a7a700ff3378e0eff911b3aa
brendt <brent.roose@gmail.com>
2025-10-21 08:35:46 +0200

    wip

--- app/Support/Sitemap/SitemapController.php
+++ app/Support/Sitemap/SitemapController.php
@@ -2,7 +2,9 @@
 
 namespace App\Support\Sitemap;
 
+use Tempest\Cache\Cache;
 use Tempest\Container\Container;
+use Tempest\DateTime\Duration;
 use Tempest\Http\Method;
 use Tempest\Http\Response;
 use Tempest\Http\Responses\Ok;
@@ -15,37 +17,43 @@ use function Tempest\view;
 final class SitemapController
 {
     #[Get('/sitemap.xml')]
-    public function __invoke(RouteConfig $routeConfig, Container $container): Response
+    public function __invoke(RouteConfig $routeConfig, Container $container, Cache $cache): Response
     {
-        $sitemap = new Sitemap();
+        $body = $cache->resolve(
+            key: 'sitemap',
+            callback: function () use ($container, $routeConfig) {
+                $sitemap = new Sitemap();
 
-        foreach ($routeConfig->staticRoutes[Method::GET->value] as $route) {
-            $uri = uri($route->handler);
+                foreach ($routeConfig->staticRoutes[Method::GET->value] as $route) {
+                    $uri = uri($route->handler);
 
-            $sitemap->uris[$uri] = $uri;
-        }
+                    $sitemap->uris[$uri] = $uri;
+                }
 
-        foreach ($routeConfig->dynamicRoutes[Method::GET->value] as $route) {
-            /** @var StaticPage $staticPage */
-            $staticPage = $route->handler->getAttribute(StaticPage::class);
+                foreach ($routeConfig->dynamicRoutes[Method::GET->value] as $route) {
+                    /** @var StaticPage $staticPage */
+                    $staticPage = $route->handler->getAttribute(StaticPage::class);
 
-            if (! $staticPage?->dataProviderClass) {
-                continue;
-            }
+                    if (! $staticPage?->dataProviderClass) {
+                        continue;
+                    }
 
-            /** @var \Tempest\Router\DataProvider $dataProvider */
-            $dataProvider = $container->get($staticPage->dataProviderClass);
+                    /** @var \Tempest\Router\DataProvider $dataProvider */
+                    $dataProvider = $container->get($staticPage->dataProviderClass);
 
-            foreach ($dataProvider->provide() as $params) {
-                $uri = uri($route->handler, ...$params);
+                    foreach ($dataProvider->provide() as $params) {
+                        $uri = uri($route->handler, ...$params);
 
-                $sitemap->uris[$uri] = $uri;
-            }
-        }
+                        $sitemap->uris[$uri] = $uri;
+                    }
+                }
 
-        $body = view(
-            'sitemap.view.php',
-            sitemap: $sitemap,
+                return view(
+                    'sitemap.view.php',
+                    sitemap: $sitemap,
+                );
+            },
+            expiration: Duration::hour(),
         );
 
         return new Ok($body)->addHeader('Content-Type', 'application/xml;charset=UTF-8');
