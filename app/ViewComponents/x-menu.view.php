68d2869e0e8126ec5858856dd63e20602ef02e7f
brendt <brent.roose@gmail.com>
2025-10-21 12:00:02 +0200

    wip

--- app/ViewComponents/x-menu.view.php
+++ app/ViewComponents/x-menu.view.php
@@ -2,10 +2,10 @@
     <h1 class="text-white text-4xl font-bold sm:text-primary text-center sm:text-left"><x-slot>stitcher.io</x-slot></h1>
 
     <div class="flex flex-wrap gap-2 justify-center sm:justify-start">
-        <x-button class="border-2 border-white" :small href="/">Blog</x-button>
-        <x-button class="border-2 border-white" :small href="/mail/archive">Newsletter</x-button>
-        <x-button class="border-2 border-white" :small href="/rss">Feed</x-button>
-        <x-button class="border-2 border-white" :small href="/books">Books</x-button>
-        <x-button class="border-2 border-white" :small href="https://tempestphp.com" class="hidden sm:block">Tempest</x-button>
+        <x-button class="sm:border-none border-2 border-white" :small href="/">Blog</x-button>
+        <x-button class="sm:border-none border-2 border-white" :small href="/mail/archive">Newsletter</x-button>
+        <x-button class="sm:border-none border-2 border-white" :small href="/rss">Feed</x-button>
+        <x-button class="sm:border-none border-2 border-white" :small href="/books">Books</x-button>
+        <x-button class="sm:border-none border-2 border-white" :small href="https://tempestphp.com" class="hidden sm:block">Tempest</x-button>
     </div>
 </div>
