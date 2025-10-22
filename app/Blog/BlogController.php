670373b9be0d517536f72aba0e5e055350b728ef
brendt <brent.roose@gmail.com>
2025-10-22 06:45:15 +0200

    wip

--- app/Blog/BlogController.php
+++ app/Blog/BlogController.php
@@ -76,6 +76,7 @@ final class BlogController
         return new Ok($xml)->addHeader('Content-Type', 'application/xml;charset=UTF-8');
     }
 
+    #[Get('/blog/{slug}/meta.png')]
     #[Get('/blog/{slug}/meta')]
     public function meta(
         string $slug,
