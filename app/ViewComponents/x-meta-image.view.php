5d8ccaf81b913a1a09e965a846d03be075526466
brendt <brent.roose@gmail.com>
2025-10-22 06:43:13 +0200

    wip

--- app/ViewComponents/x-meta-image.view.php
+++ app/ViewComponents/x-meta-image.view.php
@@ -9,8 +9,8 @@
     </style>
 </head>
 <body class="bg-[#333] flex justify-center items-center">
-<div class="w-full h-full flex justify-center items-center bg-primary relative text-white p-1">
-    <div class="w-full h-full bg-white rounded-xs text-primary flex justify-around items-center relative shadow-lg px-8">
+<div class="w-full h-full flex justify-center items-center relative">
+    <div class="w-full h-full bg-white text-primary flex justify-around items-center relative shadow-lg px-8">
         <x-slot/>
 
         <div class="absolute bottom-0 mb-3 text-xs">stitcher.io</div>
