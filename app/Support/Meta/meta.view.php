2edf6d1addf7abb706fe3872ce207dbf14879daa
brendt <brent.roose@gmail.com>
2025-11-13 10:40:17 +0100

    wip

--- app/Support/Meta/meta.view.php
+++ app/Support/Meta/meta.view.php
@@ -1,12 +1,13 @@
 <html lang="en">
 <head>
     <title>Meta</title>
-    <x-vite-tags/>
     <style>
         :root {
             font-size: 40px;
         }
     </style>
+    <x-vite-tags entrypoint="app/main.entrypoint.css"/>
+    <x-vite-tags entrypoint="app/main.entrypoint.ts"/>
 </head>
 <body class="bg-black">
 <div class="w-[1200px] h-[628px] bg-white text-white flex justify-center items-center relative p-8 bg-gradient-to-b from-red-500 to-emerald-400 border-yellow-300 border-6">
