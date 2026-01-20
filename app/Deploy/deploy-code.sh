3bea13d727b11dca7ec1cf92e84236778631271d
brendt <brent.roose@gmail.com>
2026-01-20 14:39:03 +0100

    wip

--- app/Deploy/deploy-code.sh
+++ app/Deploy/deploy-code.sh
@@ -4,17 +4,18 @@
 . ~/.nvm/nvm.sh
 
 # Dependencies
-php8.4 /usr/local/bin/composer install --no-dev
+php8.5 /usr/local/bin/composer install --no-dev
 /home/forge/.bun/bin/bun install
 
 # Backedn
-php8.4 tempest cache:clear --force --internal --all
-php8.4 tempest discovery:generate
-php8.4 tempest migrate:up --force
+php8.5 tempest cache:clear --force --internal --all
+php8.5 tempest discovery:generate
+php8.5 tempest migrate:up --force
 
 # Frontend
 /home/forge/.bun/bin/bun run build
-php8.4 tempest cache:clear --force
+php8.5 tempest cache:clear --force
+php8.5 tempest static:generate /analytics
 
 # Supervisor
 sudo supervisorctl restart all
