fe54d114a95e137b32dee0841be70919313260de
brendt <brent.roose@gmail.com>
2026-01-30 13:09:28 +0100

    wip

--- app/Deploy/deploy-code.sh
+++ app/Deploy/deploy-code.sh
@@ -7,15 +7,10 @@
 php8.5 /usr/local/bin/composer install --no-dev
 /home/forge/.bun/bin/bun install
 
-# Backedn
+# Backend
 php8.5 tempest cache:clear --force --internal --all
 php8.5 tempest discovery:generate
 php8.5 tempest migrate:up --force
-
-# Frontend
-/home/forge/.bun/bin/bun run build
-php8.5 tempest cache:clear --force
-php8.5 tempest static:generate /analytics
 
 # Supervisor
 sudo supervisorctl restart all
