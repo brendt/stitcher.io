98cafeeae4e4e395accb2ee5b8972359e1e27974
brendt <brent.roose@gmail.com>
2026-03-23 13:21:24 +0100

    WIP

--- app/Chat/chat.view.php
+++ app/Chat/chat.view.php
@@ -10,15 +10,25 @@ use App\Chat\ChatController;
     <meta charset="UTF-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover"/>
 </head>
-<body class="p-2 relative">
-
+<body class="p-2 relative flex flex-col min-h-screen">
 <div
         id="chat"
         :hx-get="uri([ChatController::class, 'realtime'])"
         hx-trigger="load, every 2s"
         hx-swap="innerHTML"
+        class="mt-auto"
 >
     <x-chat />
 </div>
+
+<script>
+    const chat = document.getElementById('chat-messages');
+    chat.scrollTop = chat.scrollHeight;
+
+    document.body.addEventListener('htmx:afterSwap', function() {
+        const chat = document.getElementById('chat-messages');
+        if (chat) chat.scrollTop = chat.scrollHeight;
+    });
+</script>
 </body>
 </html>
--- app/Chat/x-chat.view.php
+++ app/Chat/x-chat.view.php
@@ -1,4 +1,4 @@
-<div class="text-white p-4 flex flex-col-reverse gap-2 overflow-y-auto wrap-break-word bg-[#1b1429DD] rounded-lg">
+<div id="chat-messages" class="text-white p-4 flex flex-col-reverse gap-2 overflow-y-auto wrap-break-word bg-[#1b1429DD] rounded-lg max-h-[500px]">
     <div :foreach="$messages ?? [] as $message" class="flex gap-2 items-start text-shadow-gray-700 text-shadow-md">
         <span><span class="font-bold" style="color: {{ $message->color }}">{{ $message->user }}</span> {{ $message->content }}</span>
     </div>
