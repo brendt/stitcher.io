a5ce3ccbc22c70faecc4f0ac952aca632448e0f5
brendt <brent.roose@gmail.com>
2026-05-06 10:31:23 +0200

    wip

--- app/Blog/Book/x-chapter.view.php
+++ app/Blog/Book/x-chapter.view.php
@@ -1,7 +1,7 @@
 <section class="chapter" :class="'chapter-' . $chapter->index" :id="$chapter->slug">
     <h1 class="chapter-title"><a href="{{ $chapter->uri }}">{{ $chapter->title }}</a></h1>
 
-    <h2 class="chapter-date">{{ $chapter->index }} </h2>
+    <h2 class="chapter-date">Written on {{ $chapter->index }} </h2>
 
     {!! $chapter->body !!}
 </section>
