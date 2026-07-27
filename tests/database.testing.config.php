362a991547c9d876f4c8651cedc60a2448cd0e49
roxblnfk <roxblnfk@ya.ru>
2026-07-27 12:55:06 +0400

    Add .env example

--- tests/database.testing.config.php
+++ tests/database.testing.config.php
@@ -2,6 +2,12 @@
 
 use Tempest\Database\Config\MysqlConfig;
 
+use function Tempest\env;
+
 return new MysqlConfig(
-    database: 'stitcher-testing',
+    host: env('TEST_DATABASE_HOST', default: 'localhost'),
+    port: env('TEST_DATABASE_PORT', default: '3306'),
+    username: env('TEST_DATABASE_USERNAME', default: 'root'),
+    password: env('TEST_DATABASE_PASSWORD', default: ''),
+    database: env('TEST_DATABASE_DATABASE', default: 'stitcher-testing'),
 );
